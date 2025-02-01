f4e40096613e96bbd5a43cff87403e02c5ce5d80
ALIJAYA JURAGAN PULSA <37974674+alijayanet@users.noreply.github.com>
2025-02-01 15:45:55 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    // Konfigurasi untuk setiap server
+    servers: {
+        // Server 1
+        server1: {
+            name: "ALIJAYA-NET",
+            botToken: "1938127147:AAFMD7X_x7D7RIVmcgCJcKVfwEQumkVwbZU",
+            adminIds: ["567858628"],
+            customers: {},
+            genieacs: {
+                baseUrl: "http://103.175.238.126:4447",
+                username: "alijaya",
+                password: "stb2025"
+            }
+        },
+        // Tambahkan server lain sesuai kebutuhan
+    }
+}; 
